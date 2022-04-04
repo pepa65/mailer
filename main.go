08f914eaa5e06ab0ce821ad2a760155c0e2d4bde
gitlab.com/pepa65 <pepa65@passchier.net>
2022-04-04 17:40:55 +0700

    gofmt

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 )
 
 const version = "0.1.0"
+
 var defaultport = "587"
 var defaultserver = "smtp.gmail.com"
 
@@ -27,8 +28,8 @@ Usage:  mailer CONTENT MANDATORIES [OPTIONALS]
         -u|--user USER            For logging in to mail server (*)
         -p|--password PASSWORD    If PASSWORD is a dash, it is read from stdin
     OPTIONALS:
-        -S|--server SERVER        Mail server (default: ` + defaultserver + `)
-        -P|--port PORT            Port, like 25 or 465 (default: ` + defaultport + `)
+        -S|--server SERVER        Mail server (default: `+defaultserver+`)
+        -P|--port PORT            Port, like 25 or 465 (default: `+defaultport+`)
         -c|--cc EMAILS            Cc email(s)
         -b|--bcc EMAILS           Bcc email(s)
         -r|--reply EMAILS         Reply-To email(s)
@@ -36,7 +37,7 @@ Usage:  mailer CONTENT MANDATORIES [OPTIONALS]
     (*) If USER is not an email address, NAME should contain one!
     Emails can be like "you@and.me" or like "Some String <you@and.me>",
     and need to be comma-separated. Any arguments must survive shell-parsing!
-`, self, version);
+`, self, version)
 }
 
 func error(msg string) {
@@ -47,11 +48,11 @@ func error(msg string) {
 
 func extract(emails string) []string {
 	toall := strings.Split(emails, ",")
-	for i,email := range toall {
+	for i, email := range toall {
 		a := strings.Index(email, "<")
 		b := strings.Index(email, ">")
-		if a + 1 < b && a + 1 > 0  {
-			toall[i] = email[a + 1:b]
+		if a+1 < b && a+1 > 0 {
+			toall[i] = email[a+1 : b]
 		}
 	}
 	return toall
@@ -59,12 +60,12 @@ func extract(emails string) []string {
 
 func main() {
 	// Use name of binary/link
-  self = os.Args[0]
-  i := strings.IndexByte(self, '/')
-  for i >= 0 {
-    self = self[i + 1:]
-    i = strings.IndexByte(self, '/')
-  }
+	self = os.Args[0]
+	i := strings.IndexByte(self, '/')
+	for i >= 0 {
+		self = self[i+1:]
+		i = strings.IndexByte(self, '/')
+	}
 
 	// Usage on no arguments
 	nArgs := len(os.Args)
@@ -75,124 +76,124 @@ func main() {
 
 	// Parse commandline
 	i = 1
-	from,to,subject,user,password,server,port,cc,bcc,reply,message,file := "","","","","","","","","","","",""
+	from, to, subject, user, password, server, port, cc, bcc, reply, message, file := "", "", "", "", "", "", "", "", "", "", "", ""
 	for i < nArgs {
 		switch os.Args[i] {
-		case "-f","--from":
+		case "-f", "--from":
 			if from != "" {
 				error("Can't use -f/--from twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -f/--from must have an argument")
 			}
-			from = os.Args[i + 1]
+			from = os.Args[i+1]
 			i = i + 1
-		case "-t","--to":
+		case "-t", "--to":
 			if to != "" {
 				error("Can't use -t/--to twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -t/--to must have an argument")
 			}
-			to = os.Args[i + 1]
+			to = os.Args[i+1]
 			i = i + 1
-		case "-s","--subject":
+		case "-s", "--subject":
 			if subject != "" {
 				error("Can't use -s/--subject twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -s/--subject must have an argument")
 			}
-			subject = os.Args[i + 1]
+			subject = os.Args[i+1]
 			i = i + 1
-		case "-u","--user":
+		case "-u", "--user":
 			if user != "" {
 				error("Can't use -u/--user twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -u/--user must have an argument")
 			}
-			user = os.Args[i + 1]
+			user = os.Args[i+1]
 			i = i + 1
-		case "-p","--password":
+		case "-p", "--password":
 			if password != "" {
 				error("Can't use -p/--password twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -p/--password must have an argument")
 			}
-			password = os.Args[i + 1]
+			password = os.Args[i+1]
 			i = i + 1
-		case "-S","--server":
+		case "-S", "--server":
 			if server != "" {
 				error("Can't use -S/--server twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -S/--server must have an argument")
 			}
-			server = os.Args[i + 1]
+			server = os.Args[i+1]
 			i = i + 1
-		case "-P","--port":
+		case "-P", "--port":
 			if port != "" {
 				error("Can't use -P/--port twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -P/--port must have an argument")
 			}
-			port = os.Args[i + 1]
+			port = os.Args[i+1]
 			i = i + 1
-		case "-c","--cc":
+		case "-c", "--cc":
 			if cc != "" {
 				error("Can't use -c/--cc twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -c/--cc must have an argument")
 			}
-			cc = os.Args[i + 1]
+			cc = os.Args[i+1]
 			i = i + 1
-		case "-b","--bcc":
+		case "-b", "--bcc":
 			if bcc != "" {
 				error("Can't use -b/--bcc twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -b/--bcc must have an argument")
 			}
-			bcc = os.Args[i + 1]
+			bcc = os.Args[i+1]
 			i = i + 1
-		case "-r","--reply":
+		case "-r", "--reply":
 			if reply != "" {
 				error("Can't use -r/--reply twice")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -r/--reply must have an argument")
 			}
-			reply = os.Args[i + 1]
+			reply = os.Args[i+1]
 			i = i + 1
-		case "-m","--message":
+		case "-m", "--message":
 			if message != "" {
 				error("Can't use -m/--message twice")
 			}
 			if file != "" {
 				error("Can't use both -m/--message and -F/--file flags")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -m/--message must have an argument")
 			}
-			message = os.Args[i + 1]
+			message = os.Args[i+1]
 			i = i + 1
-		case "-F","--file":
+		case "-F", "--file":
 			if file != "" {
 				error("Can't use -F/--file twice")
 			}
 			if message != "" {
 				error("can't use both -m/--message and -F/--file flags")
 			}
-			if i + 2 > len(os.Args) {
+			if i+2 > len(os.Args) {
 				error("Flag -F/--file must have an argument")
 			}
-			file = os.Args[i + 1]
+			file = os.Args[i+1]
 			i = i + 1
-		case "-h","--help":
+		case "-h", "--help":
 			usage()
 			return
 		default:
@@ -247,10 +248,10 @@ func main() {
 
 	// Assemble email with headers and body
 	body := "To: " + to + "\nFrom: " + from + "\n"
-	if reply !="" {
+	if reply != "" {
 		body = body + "Reply-To: " + reply + "\n"
 	}
-	if cc !="" {
+	if cc != "" {
 		body += "Cc: " + cc + "\n"
 	}
 	body += "Subject: " + subject + "\n\n"
@@ -266,7 +267,7 @@ func main() {
 
 	// Send with net/smtp.sendmail
 	auth := smtp.PlainAuth("", user, password, server)
-	err := smtp.SendMail(server + ":" + port, auth, user, toall, []byte(body))
+	err := smtp.SendMail(server+":"+port, auth, user, toall, []byte(body))
 	if err != nil { // No news is good news
 		fmt.Println("Error: ", err)
 	}
